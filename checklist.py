5da01ccf4b2c135de4d8b863e11ceeecdbb71291
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-05-07 23:47:56 +0600

    added checklist file

---
+++ checklist.py
@@ -0,0 +1,13 @@
+# a = 50
+# b = 60
+# result = a + b
+# print(result)
+# print("The summation is", result)
+
+# first_name = "Mahbubur"
+# last_name = "Rahman"
+# full_name = first_name + " " + last_name
+# print("Your full name: " + full_name)
+
+
+
